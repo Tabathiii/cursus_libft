e5db5509b3aca7251a4e73ea9074a3718f8967a0
Tabathiii <shushutbt@gmail.com>
2026-04-30 14:36:14 +0900

    Update ft_memset.c

--- ft_memset.c
+++ ft_memset.c
@@ -1,1 +1,17 @@
-void	*memset(void *s, int c, size_t n);
+#include <string.h>
+
+void	*memset(void *s, int c, size_t n)
+{
+    unsigned char   *p;
+    unsigned char   q;
+
+    p = (unsigned char  *)s;
+    q = (unsigned char)c;
+    while (n > 0)
+    {
+        *p = q;
+        p++;
+        n--;
+    }
+    return (s);
+}
