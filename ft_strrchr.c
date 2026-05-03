20867e22b5b91156e3dbecf03efbd17badba2088
Tabathiii <shushutbt@gmail.com>
2026-05-04 04:20:15 +0900

    Update ft_strrchr.c

---
+++ ft_strrchr.c
@@ -0,0 +1,13 @@
+char	*ft_strrchr(const char *s, int c)
+{
+	int	len;
+	
+	len = ft_strlen(s);
+	while (len >= 0)
+	{
+		if (s[len] == (char)c)
+			return ((char *)s + len);
+		len--;
+	}
+	return (NULL);
+}
