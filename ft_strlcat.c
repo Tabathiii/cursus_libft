40d42e95acc7022ab027a534773a8503e39d397f
Tabathiii <shushutbt@gmail.com>
2026-05-07 22:52:11 +0900

    Update ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,16 +1,21 @@
-size_t  ft_strlcat(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-    size_t  dst_len;
-    size_t  str_len;
-    size_t  i;
+	size_t	dst_len;
+	size_t	src_len;
+	size_t	i;
 
-    dst_len = ft_strlen(dst);
-    src_len = ft_strlen(src);
-    if (dst_len >= dstsize)
-        return (src_len + dstsize);
-    dst += dst_len;
-    i = dst_len
-    while (i < dstsize - 1)
-    {
-    }
+	dst_len = 0;
+	while (dst[dst_len] != '\0' && dst_len < dstsize)
+		dst_len++;
+	src_len = ft_strlen(src);
+	if (dst_len >= dstsize)
+		return (src_len + dstsize);
+	i = dst_len;
+	while (i < dstsize - 1 && src[i - dst_len] != '\0')
+	{
+		dst[i] = src[i - dst_len];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst_len + src_len);
 }
