facb73e8609982c1534ec02932793efa67c4100b
Tabathiii <shushutbt@gmail.com>
2026-05-09 17:24:20 +0900

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,4 +1,13 @@
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if ((unsigned char)s1[i] != (unsigned char)s2[i] || s1[i] == '\0')
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+	return (0);
 }
-// 文字列比較（戻り値はアスキーの差）
