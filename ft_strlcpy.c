bc2bddf6c24e58f8ef40ef88a94b00f503d82ea4
Tabathiii <shushutbt@gmail.com>
2026-05-09 16:19:18 +0900

    Update ft_strlcpy.c

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,4 +1,19 @@
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
+	size_t	src_len;
+	size_t	i;
+
+	src_len = ft_strlen(src);
+	i = 0;
+	if (dstsize == 0)
+		return (src_len);
+	while (i + 1 < dstsize && src[i] != '\0')
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (src_len);
 }
 // サイズ制限付きの文字列コピー
+// 場合分けでクラッシュ対策
