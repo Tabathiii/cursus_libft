ed208600b3d9f10903778cd4b223257335d2196f
Tabathiii <shushutbt@gmail.com>
2026-05-11 20:17:24 +0900

    Update ft_strnstr.c wirting

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,4 +1,14 @@
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
+	unsigned char	*start;
+	size_t			i;
+	size_t			need_len;
+
+	start = (unsigned char *)haystack;
+	need_len = ft_strlen(needle);
+	while (i < len && *(start + need_len - 1) != '\0')
+	{
+		while ()
+	}
 }
 // 文字列検索
