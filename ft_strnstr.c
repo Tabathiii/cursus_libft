79edc6e7d915508abf3af7a2a28bd432b209f342
Tabathiii <shushutbt@gmail.com>
2026-05-17 12:46:38 +0900

    Update ft_strnstr.c wait for checking

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,14 +1,19 @@
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	unsigned char	*start;
 	size_t			i;
-	size_t			need_len;
 
-	start = (unsigned char *)haystack;
-	need_len = ft_strlen(needle);
-	while (i < len && *(start + need_len - 1) != '\0')
+	if (*needle == '\0')
+		return ((char *)haystack);
+	while (len > 0 && *haystack != '\0')
 	{
-		while ()
+		i = 0;
+		while (i < len && needle[i] != '\0' && haystack[i] == needle[i])
+			i++;
+		if (needle[i] == '\0')
+			return ((char *)haystack);
+		haystack++;
+		len--;
 	}
+	return (NULL);
 }
 // 文字列検索
