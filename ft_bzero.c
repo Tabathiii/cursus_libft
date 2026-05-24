4dac16ae8d5cbe299afdbf91de732cbe809c7343
Tabathiii <shushutbt@gmail.com>
2026-05-24 17:45:28 +0900

    Update ft_bzero.c

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,11 +1,10 @@
-// #include <stdio.h>
-#include <string.h>
+#include "libft.h"
 
 void	ft_bzero(void *s, size_t n)
 {
 	unsigned char	*p;
 
-	p = (unsigned char	*)s;
+	p = (unsigned char *)s;
 	while (n > 0)
 	{
 		*p = 0;
@@ -13,21 +12,3 @@ void	ft_bzero(void *s, size_t n)
 		n--;
 	}
 }
-
-// int	main(void)
-// {
-// 	char	str[] = "HELLO";
-// 	size_t	num;
-// 	int	i;
-
-// 	num = 3;
-// 	i = 0;
-// 	ft_bzero(str, num);
-// 	while (i < 5)
-// 	{
-// 		printf("%d ", str[i]);
-// 		i++;
-// 	}
-// 	return (0);
-// }
-
