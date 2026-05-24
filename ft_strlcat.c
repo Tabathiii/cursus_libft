531a34ae3374b14e3ccc61cb01b862b46d695232
Tabathiii <shushutbt@gmail.com>
2026-05-24 17:51:14 +0900

    Update ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,3 +1,5 @@
+#include "libft.h"
+
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	dst_len;
@@ -19,3 +21,5 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	dst[i] = '\0';
 	return (dst_len + src_len);
 }
+// サイズ制限付きの文字列連結
+//dstに'\0'が無い時対策
