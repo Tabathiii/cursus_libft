d07bbc17cca5e5b045b2dc430ad72b7415dab324
Tabathiii <shushutbt@gmail.com>
2026-05-24 17:44:59 +0900

    Update ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,5 +1,4 @@
-// #include <stdio.h>
-#include <stdlib.h>
+#include "libft.h"
 
 int	ft_atoi(const char *str)
 {
@@ -25,21 +24,3 @@ int	ft_atoi(const char *str)
 	}
 	return (num * j);
 }
-
-// int	main(void)
-// {
-// 	char	*a;
-// 	char	*b;
-// 	char	*c;
-// 	int	num1;
-// 	int	num2;
-// 	int	num3;
-
-// 	a = "  +1234dsf";
-// 	b = "  	-1234sdv";
-// 	c = "  	-+1234dry";
-// 	num1 = ft_atoi(a);
-// 	num2 = ft_atoi(b);
-// 	num3 = ft_atoi(c);
-// 	printf("%d, %d, %d", num1, num2, num3);
-// }
