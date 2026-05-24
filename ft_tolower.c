db5088aec7eab4396233f5c8fc0ebb84f5b7e2aa
Tabathiii <shushutbt@gmail.com>
2026-05-24 17:52:59 +0900

    Update ft_tolower.c

--- ft_tolower.c
+++ ft_tolower.c
@@ -1,6 +1,8 @@
-int ft_tolower(int c)
+#include "libft.h"
+
+int	ft_tolower(int c)
 {
-    if (c >= 'A' && c <= 'Z')
-        return (c + ('a' - 'A'));
-    return (c);
+	if (c >= 'A' && c <= 'Z')
+		return (c + ('a' - 'A'));
+	return (c);
 }
