d234efec504d81aae21a9af77d3ae0fe67650e3c
Tabathiii <shushutbt@gmail.com>
2026-06-20 16:55:06 +0900

    Update ft_substr.c writting

--- ft_substr.c
+++ ft_substr.c
@@ -1,33 +1,34 @@
 #include "libft.h"
 
-char    *ft_substr(const char *s, unsigned int start, size_t len)
+char	*ft_substr(const char *s, unsigned int start, size_t len)
 {
-    unsigned char   *sub;
-    size_t          s_len;
+	char	*sub;
+	size_t	s_len;
+	size_t	i;
 
-    if (!s)
-        return (NULL);
-    s_len = ft_strlen(s);
-    if (start >= s_len)
-    {
-        sub = malloc(1);
-        if (!sub)
-            return (NULL);
-        sub[0] = '\0';
-        return ((char *)sub);
-    }
-    sub = malloc(sizeof(char) * (s_len + 1));
-    if (!sub)
-        return (NULL);
-    s += start;
-    while (s_len != 0)
-    {
-        *sub = *s;
-        sub++;
-        s++;
-        s_len--;
-    }
-    *sub = '\0';
-    return ((char *)sub);
+	if (!s)
+		return (NULL);
+	s_len = ft_strlen(s);
+	i = 0;
+	if (start >= s_len)
+	{
+		sub = malloc(1);
+		if (!sub)
+			return (NULL);
+		sub[0] = '\0';
+		return (sub);
+	}
+	if (start + len > s_len)
+		len = s_len - start;
+	sub = malloc(sizeof(char) * (len + 1));
+	if (!sub)
+		return (NULL);
+	while (i < len)
+	{
+		sub[i] = s[i + start];
+		i++;
+	}
+	sub[i] = '\0';
+	return (sub);
 }
 //文字列から数文字引き出して返す関数だから文字数を測るstrlenでおっけー
